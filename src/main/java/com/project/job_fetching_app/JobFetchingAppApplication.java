6310f5a64f5cc67ef21b9c26db7effcdbed18a1a
Dicle Kilic <diclekilic79@gmail.com>
2024-06-24 19:38:46 +0300

    Initial commit

---
+++ src/main/java/com/project/job_fetching_app/JobFetchingAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.job_fetching_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobFetchingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobFetchingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/job_fetching_app/JobFetchingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.job_fetching_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobFetchingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
